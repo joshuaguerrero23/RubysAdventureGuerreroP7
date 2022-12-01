15b39f2630107bc0dd6979f6cd806be6e626c194
joshuaguerrero23 <joshtheframe@gmail.com>
2022-12-01 15:25:37 -0800

    Configured Ruby Controller
    Configured Ruby's controller script and made health bar

--- RubysAdventureGuerrero/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuerrero/Assets/Scripts/RubyController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizantal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 240;
         
     }
 
@@ -14,16 +25,27 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         //create variables to use the built in axes
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizantal");
         float vertical = Input.GetAxis("Vertical");
 
         //Create a movement vector
+    }
+    void FixedUpdate()
+    {
+   
         Vector2 position = transform.position;
 
         //make vertical and horizontal movement work
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.x = position.x + 3.0f * horizantal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
+        rigidbody2d.MovePosition(position);
+
         transform.position = position;
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
